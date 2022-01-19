742a3030b8f2e6e30964cb8654a4d4ee564fe257
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2022-01-19 16:31:01 +0300

    Create test.py

---
+++ test_model.py
@@ -0,0 +1,37 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello UrFU"}
+
+def test_read_predict_positive():
+    response = client.post("/predict/",
+        json={"text": "Всё прекрасно!"}
+    )
+    json_data = response.json() 
+
+    assert response.status_code == 200
+    assert json_data['label'] == 'POSITIVE'
+
+def test_read_predict_negative():
+    response = client.post("/predict/",
+        json={"text": "Ужасная,безвыходная ситуация с пректом!"}
+    )
+    json_data = response.json() 
+
+    assert response.status_code == 200
+    assert json_data['label'] == 'NEGATIVE'
+    
+def test_read_predict_neutral():
+    response = client.post("/predict/",
+        json={"text": "Привет!"}
+    )
+    json_data = response.json() 
+
+    assert response.status_code == 200
+    assert json_data['label'] == 'NEUTRAL'
